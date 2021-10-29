50544a45f8b98dd4fb70c582b02da60916c6b079
kksswkd <kksswkd@gmail.com>
2021-10-29 15:55:22 +0900

    Add: image up

--- web/config/db_config.js
+++ web/config/db_config.js
@@ -8,8 +8,7 @@ const db_info = {
 	dev:{
 		host: 'localhost',
 		user: 'root',
-		// password: process.env.DB_SECRET, 
-		password: '0000', 
+		password: process.env.DB_SECRET,
 		database: 'capstone',
 		multipleStatements: true
     },
